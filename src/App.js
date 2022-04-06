f93245ff6fd42e32a14fee727810ed38aa1b6152
Oluwadamilola Gbotolorun <damigbot@gmail.com>
2022-04-06 13:32:59 +0100

    initialize project create components directory create Layout directory create pages directory create meetups directory

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Redirect, Route, Switch } from "react-router-dom";
+import MainNavigation from "./components/layout/MainNavigation";
+import AllMeetups from "./pages/AllMeetups";
+import Favorites from "./pages/Favorites";
+import NewMeetups from "./pages/NewMeetups";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<MainNavigation />
+			<Switch>
+				<Route path={"/"} exact>
+					<AllMeetups />
+				</Route>
+
+				<Route path={"/new-meetup"}>
+					<NewMeetups />
+				</Route>
+
+				<Route path={"/favorites"}>
+					<Favorites />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/Favorites.js
@@ -0,0 +1,5 @@
+const Favorites = () => {
+	return <h1>This is the Favorites Page</h1>;
+};
+
+export default Favorites;
