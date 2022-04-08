9408ce6b2146b4a980b4773a55262b4b5c97baee
Oluwadamilola Gbotolorun <damigbot@gmail.com>
2022-04-08 15:13:39 +0100

    fetch and render meetup data from a server

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,32 +1,44 @@
+import { useEffect, useState } from "react";
+
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_DATA = [
-	{
-		id: "m1",
-		title: "This is a first meetup",
-		image:
-			"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-		address: "Meetupstreet 5, 12345 Meetup City",
-		description:
-			"This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-	},
-	{
-		id: "m2",
-		title: "This is a second meetup",
-		image:
-			"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-		address: "Meetupstreet 5, 12345 Meetup City",
-		description:
-			"This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-	},
-];
-
 const AllMeetups = () => {
+	const [meetupData, setMeetupData] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		setIsLoading(true);
+		fetch(
+			"https://react-getting-started-e621a-default-rtdb.firebaseio.com/meetups.json"
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				const transformedValue = Object.values(data);
+				const transformedKey = Object.keys(data);
+
+				const transformedData = transformedValue.map((data, index) => {
+					data.id = transformedKey[index];
+					return data;
+				});
+
+				setIsLoading(false);
+				setMeetupData(transformedData);
+			});
+	}, []);
+
+	if (isLoading) {
+		return (
+			<section>
+				<h2 style={{ textAlign: "center" }}>Loading....</h2>
+			</section>
+		);
+	}
+
 	return (
 		<section>
 			<h1>All Meetups</h1>
 
-			<MeetupList meetups={DUMMY_DATA} />
+			<MeetupList meetups={meetupData} />
 		</section>
 	);
 };
